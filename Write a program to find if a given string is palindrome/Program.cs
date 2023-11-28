ab3d2e24671ca7393b90532b292aec46651b86ec
NAJIMdp41 <143899549+najimpinjari@users.noreply.github.com>
2023-11-28 23:53:57 +0530

    Add files via upload

---
+++ Write a program to find if a given string is palindrome/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Write_a_program_to_find_if_a_given_string_is_palindrome
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.Write("Enter a string to reverse: ");
+            string input = Console.ReadLine();
+
+            string reversed = ReverseString(input);
+
+            Console.WriteLine($"Reversed string: {reversed}");
+        }
+
+        static string ReverseString(string input)
+        {
+            char[] charArray = input.ToCharArray();
+            Array.Reverse(charArray);
+            return new string(charArray);
+        }
+    }
+}
