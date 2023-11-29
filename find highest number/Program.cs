c3bfba4efa097b4ee252becb41418c5f441e2810
NAJIMdp41 <143899549+najimpinjari@users.noreply.github.com>
2023-11-29 10:15:58 +0530

    Add files via upload

---
+++ find highest number/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace find_highest_number
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[]number = new int[] { 12, 45, 212, 566, 87, 1, 90 };
+
+            int higest = 0;
+            for(int i = 0; i < number.Length; i++)
+            {
+                for(int j =i+1; j< number.Length; j++)
+                {
+                    if (number[j] > higest)
+                    {
+                        higest = number[j];
+                    }
+                }
+            }
+            Console.WriteLine($" highest number ; {higest}");
+
+
+           
+
+            Console.ReadLine();
+        }
+    }
+}
