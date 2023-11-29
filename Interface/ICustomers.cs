05657982695c299c5886afcfbd19366835695515
NAJIMdp41 <143899549+najimpinjari@users.noreply.github.com>
2023-11-29 18:08:20 +0530

    Add files via upload

---
+++ Interface/ICustomers.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Interface
+{
+    internal interface ICustomers //interface 
+    {
+        void showtiming(); //method
+
+        void printTicke();
+
+    }
+    public class silvercustomers : ICustomers //silvercust is impliment the interface after that wi can 
+        //create a obj of the class 
+    {
+        public void printTicke()
+        {
+            Console.WriteLine("silvercustomers ; printTicke(); //method ");
+        }
+        //they both are method 
+        public void showtiming()
+       {
+            Console.WriteLine("silvercustomers ; showtiming(); //method ");
+       }
+    }
+
+    public class GoldCustomer : ICustomers
+    {
+        public void printTicke()
+        {
+            Console.WriteLine("goldcustomer ; printTicke(); //method");
+        }
+        //they both are method 
+        public void showtiming()
+        {
+            Console.WriteLine("goldcustomer ; showtiming(); //method");
+        }
+    }
+}
---
+++ Interface/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Interface
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Icustomer c1 = new ICustomer(); //not possible 
+            /*
+                        ICustomers c1  = new silvercustomers();
+                        c1.showtiming(); //callin method 
+                        c1.printTicke();
+
+                        c1 = new GoldCustomer();
+                        c1.showtiming();//calling method
+                        c1.printTicke();  
+
+                        silvercustomers sc = new silvercustomers();
+                        sc.showtiming();
+            */
+
+            ICustomers[] customers = new ICustomers[2];
+            customers[0] = new silvercustomers();
+            customers[1] = new GoldCustomer();   
+
+            for(int i  = 0; i <customers.Length; i++)
+            {
+                customers[i].showtiming();
+                customers[i].printTicke(); 
+            }
+           
+
+
+           Console.ReadLine();
+        }
+    }
+}
