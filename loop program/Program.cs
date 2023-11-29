e08dbf6f16d9a88a6f8b39c8476541335afd60cc
NAJIMdp41 <143899549+najimpinjari@users.noreply.github.com>
2023-11-29 10:16:28 +0530

    Add files via upload

---
+++ loop program/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace loop_program
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Enter yout number");
+           /* int num = int.Parse(Console.ReadLine());
+
+            for(int i = num-1; i>0; i--)
+            {
+                Console.WriteLine(i);
+            }*/
+           int n = int.Parse(Console.ReadLine());
+
+            for(int i = 1; i < n; i++)
+            {
+                if (i % 2 != 0)
+                {
+                    Console.Write(i + " ");
+                }
+
+            }
+            for(int i = 1; i <= n; i++)
+            {
+                if(i % 2 == 0)
+                {
+                    Console.Write(i + " ");
+                }
+            }
+            Console.WriteLine();
+
+
+
+
+
+
+            Console.ReadLine();
+        }
+    }
+}
