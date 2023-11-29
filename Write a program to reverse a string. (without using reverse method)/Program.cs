cf5038a7a083f00f72eb3a498a34daf71e7ef417
NAJIMdp41 <143899549+najimpinjari@users.noreply.github.com>
2023-11-29 18:43:00 +0530

    Add files via upload

---
+++ Write a program to reverse a string. (without using reverse method)/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Write_a_program_to_reverse_a_string._without_using_reverse_method_
+{
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("please enter a sentence ");
+            string sentence = Console.ReadLine();
+
+            string[] words = sentence.Split(new char[] {' '});
+
+          //  Console.WriteLine("OUTPUT ; \n");
+            for(int i = words.Length - 1; i >= 0; i--)
+            {
+                Console.Write($"{words[i]}");
+            }
+
+
+
+
+
+
+        }
+    }
+
+
+/*  internal class Program
+  {
+      static void Main(string[] args)
+      {
+
+          Console.WriteLine("Please enter your name");
+          string name = Console.ReadLine();
+
+          string revercename = string.Empty;
+
+          for(int i = name.Length -1 ; i  >= 0 ; i--)
+          {
+              revercename += name[i];
+          }
+          Console.WriteLine($" input {name} ; revercename {revercename}");
+
+      }
+  }*/
+}
