41c24e5af6991bd476e9990634ed3c2bd9008631
NAJIMdp41 <143899549+najimpinjari@users.noreply.github.com>
2023-11-30 17:50:39 +0530

    Add files via upload

---
+++ 1.Math Library/A.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1.Math_Library
+{
+    public class A
+    {
+        public void PrintA()
+        {
+            Console.WriteLine("PrinA()");
+        }
+        
+    }
+     public class b : A
+    {
+        private void Print()
+        {
+          //  A a = new A();
+          //  a.Print();
+             
+            Console.WriteLine("Printb()");
+        }
+    }
+    public class C 
+    {
+        private void Print()
+        {   
+            A a = new A();
+            a.PrintA();
+
+            Console.WriteLine("PrintC()");
+        }
+
+    }
+}
---
+++ 1.Math Library/Calculator.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace _1.Math_Library
+{
+    public class Calculator
+    {
+        public void add(int a, int b)
+        {
+            Console.WriteLine($" addition {a} + {b} = {a + b}");
+        }
+    }
+}
