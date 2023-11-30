b3cfd36db182b33690690b82d0c7b596c19394f2
NAJIMdp41 <143899549+najimpinjari@users.noreply.github.com>
2023-11-30 18:02:54 +0530

    Add files via upload

---
+++ Math library/Calculator.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Math_library
+{
+    internal class Calculator
+    {
+        public void add(int a, int b)
+        {
+            Console.WriteLine($" addition {a} + {b} = {a + b}"); 
+        }
+    }
+}
