45d3a4b5ea02796691fb90049a4cafae0c967370
NAJIMdp41 <143899549+najimpinjari@users.noreply.github.com>
2023-11-30 18:02:34 +0530

    Add files via upload

---
+++ 1.Math Library/MathLabraryClient/Calculator.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MathLabraryClient
+{
+    internal class Calculator
+    {
+        internal void add(int first, int second)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ 1.Math Library/MathLabraryClient/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MathLabraryClient
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("welcome to calculator application ");
+
+            Calculator c1 = new Calculator();
+            Console.WriteLine("Please enter first number  ");
+            int first = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("enter yout second number");
+            int second = int.Parse(Console.ReadLine());
+
+            c1.add(first, second);
+
+            Console.WriteLine("thank you for using calculator application ");
+
+            Console.ReadLine();
+        }
+    }
+}
