6c3d05b184acbdaeb598326a817afed3eece5442
NAJIMdp41 <143899549+najimpinjari@users.noreply.github.com>
2023-12-05 18:09:32 +0530

    Add files via upload

---
+++ Company_ibrary/Company.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Company_ibrary
+{
+    public  class Company
+    {
+        public int companyId { get; set; }
+        public string name { get; set; }
+
+        public Employe[] Employees { get; set; }
+
+
+        public Company(Employee[] emps)
+        {
+            Employees = emps;
+        }
+        public void details()
+        {
+            Console.WriteLine($" companyid : {companyId} name : {name}");
+        }
+
+        public void AllEmployee()
+        { 
+            if (Employe != null && Employees.Length  > 0  )    
+            {
+                for(int i = 0; i < Employe.Length; i++)
+                {
+                    Console.WriteLine($"Employeeid  ; {Employe[i].Employeeid} name : {employees[i].Name} city: {employees[i].city} experience : {employees[i].experience}");    
+                }
+            }
+            else
+            {
+                Console.WriteLine("no employee in company ");
+            }
+        
+        }
+    }
+}
---
+++ Company_ibrary/Employee.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Company_ibrary
+{
+    public class employee   
+    {
+        public int Employeeid { get; set; }
+        public string Name { get; set; }
+        public string city { get; set; }
+        public int experience { get; set; }
+
+        public Company com { get; set; }
+        public Company Company { get; set; }
+
+        public void Details()
+        {
+            Console.WriteLine($"Employeeid : {Employeeid}  name : {Name} city : {city} experience : {experience}"); 
+        }
+    }
+}
