060293e9b7e963c6ff5210f3aa7394d1269119ad
NAJIMdp41 <143899549+najimpinjari@users.noreply.github.com>
2023-12-06 17:55:08 +0530

    Add files via upload

---
+++ MyException/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyException
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int i = 10;
+            Console.WriteLine("enter your number ");
+            int j = int.Parse(Console.ReadLine());
+            int result = 0;
+            try
+            {
+                result = i / j;
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("divide by 0 is not allowed  ");
+            }
+            
+            Console.WriteLine($"result is = : {result}");
+
+
+
+            Console.ReadLine();
+        }
+    }
+}
