dc91b7a74b42dcb185acae9553ecd8e9c4afe58a
NAJIMdp41 <143899549+najimpinjari@users.noreply.github.com>
2023-12-07 17:26:01 +0530

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Reverse_a_String
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("enter a string");
+            string input  = Console.ReadLine(); 
+
+            char[] charArry = input.ToCharArray();
+            Array.Reverse(charArry);
+
+            string Reverce = new string(charArry);
+            Console.WriteLine($"reverce Array {Reverce}");
+
+            Console.ReadLine();
+        }
+    }
+}
