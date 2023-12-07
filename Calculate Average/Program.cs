0a7746f764c8286c8a33b027fd4270e1b8144a5e
NAJIMdp41 <143899549+najimpinjari@users.noreply.github.com>
2023-12-07 17:25:11 +0530

    Add files via upload

---
+++ Calculate Average/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculate_Average
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter the number of elements : ");
+            int n = int.Parse(Console.ReadLine());
+
+            double sum  = 0;
+
+            for(int i = 0; i < n; i++)
+            {
+                Console.WriteLine($"enter number {i + 1 }: ");
+                double num = double .Parse(Console.ReadLine());
+                sum += num;
+            }
+            double average = sum / n;
+            Console.WriteLine($"Average : {average}");
+
+        }
+    }
+}
