9223b78a175bbba41d2f64087ae225b01f0ba0b5
NAJIMdp41 <143899549+najimpinjari@users.noreply.github.com>
2023-12-07 17:26:23 +0530

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -4,22 +4,52 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Reverse_a_String
+namespace Exception_handling_continew
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("enter a string");
-            string input  = Console.ReadLine(); 
+            Console.WriteLine("welcome to calculator application");
+            Console.WriteLine("please enter numearator");
+            int  numerator , denominator ;
 
-            char[] charArry = input.ToCharArray();
-            Array.Reverse(charArry);
+            if (int.TryParse(Console.ReadLine(), out numerator))
+            {
+                Console.WriteLine("please enter denominator");
 
-            string Reverce = new string(charArry);
-            Console.WriteLine($"reverce Array {Reverce}");
+                if (int.TryParse(Console.ReadLine(), out denominator))
+                {
+                    Divid(numerator , denominator);
+                }
+                else
+                {
+                    Console.WriteLine("Please enter a valid denominator");
+                }
+            }
+            else
+            {
+                Console.WriteLine("please enter valid numar");
+            }
+            Console.WriteLine("Thank you visit again ");
 
             Console.ReadLine();
         }
+        static void Divid(int num, int den)
+        {
+            if(den != 0)
+            {
+                int div = num / den;
+                Console.WriteLine($"division {num} / {den} int {den}");
+            }
+            else
+            {
+                Console.WriteLine("denominator cannot be zero");
+            }
+          
+            Console.WriteLine("division opretion completed");
+        }
+
     }
+   
 }
