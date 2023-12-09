498c06d0f25201a100607d3db6e9912c28b5fe38
NAJIMdp41 <143899549+najimpinjari@users.noreply.github.com>
2023-12-09 15:45:44 +0530

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -1,38 +1,34 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Runtime.InteropServices;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Largest_of_Three_Numbers
+namespace Character_Repeater
 {
     class Program
     {
-        public static void Main()
+        static void Main()
         {
-            Console.WriteLine("largest of three numbers ");
+            Console.WriteLine("charector repited ");
 
-            Console.WriteLine("enter the first number ");
-            int num1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter a charechter ");
+            char charechter = Convert.ToChar(Console.ReadLine());
 
-            Console.WriteLine("enter the second number");
-            int num2 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("enter the number of time to repeat : ");
+            int repeatcount = int.Parse(Console.ReadLine());
 
-            Console.WriteLine("enter the third number");
-            int num3 = Convert.ToInt32(Console.ReadLine());
+            string repeatedstring = Repeatcharector(charechter, repeatcount);
 
-            int largest = Findlargestnumber (num1, num2, num3);
-
-            Console.WriteLine($" the largest number is {largest}");
+            Console.WriteLine($" result {repeatedstring}");
 
             Console.ReadLine();
+
         }
-        static int Findlargestnumber(int a, int b, int c)
+        static string Repeatcharector(char ch, int count)
         {
-            return Math.Max(Math.Max(a, b) , c);
+            return new string(ch, count);
         }
-
     }
-
+  
 }
