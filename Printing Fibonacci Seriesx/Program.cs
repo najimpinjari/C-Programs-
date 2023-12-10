642713e630145ea6bed8abc4c4f6a1ff25b76937
NAJIMdp41 <143899549+najimpinjari@users.noreply.github.com>
2023-12-10 17:46:06 +0530

    Add files via upload

---
+++ Printing Fibonacci Seriesx/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Printing_Fibonacci_Seriesx
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n, a = 0, b = 1, c;
+
+            Console.WriteLine("Enter the number of terms : ");
+            n = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("fibonacci series ; ");
+            Console.WriteLine($" {0} {1}", a, b);
+
+            for (int i = 0; i < n; i++)
+            {
+                c = a + b;
+                Console.WriteLine(c);
+                a = b;
+                b = c;
+
+            }
+
+        }
+    }
+}
