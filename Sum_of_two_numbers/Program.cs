b16c7d0639ed247838a5584a3f924432fa604fa0
NAJIMdp41 <143899549+najimpinjari@users.noreply.github.com>
2023-12-10 17:47:07 +0530

    Add files via upload

---
+++ Sum_of_two_numbers/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sum_of_two_numbers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int num1, num2, sum;
+
+            Console.WriteLine("Enter first number : ");
+            num1 = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Enter second number : ");
+            num2 = int.Parse(Console.ReadLine());
+
+            sum = num1 + num2;
+
+            Console.WriteLine($"Sum of {num1} and {num2} is: {sum}");
+
+
+            Console.ReadLine();           
+        }
+    }
+}
