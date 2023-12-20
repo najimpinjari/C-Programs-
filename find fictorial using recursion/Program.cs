d262b7e85a490577c07f2d1bca7ec19bb86224e9
NAJIMdp41 <143899549+najimpinjari@users.noreply.github.com>
2023-12-20 18:32:45 +0530

    Add files via upload

---
+++ find fictorial using recursion/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace find_fictorial_using_recursion
+{
+    class Program
+    {
+        int num = 5;
+        int result = 1;
+        
+        int factorial()
+        {
+            if(num == 0)
+            {
+                return 1 ;
+            }
+            result = result * num;
+            num--;
+            factorial();
+            return result;
+        }
+        static void Main(string[] args)
+        {
+            Program obj = new Program();
+            int factorial =  obj.factorial();
+            Console.WriteLine(factorial);
+
+            Console.ReadLine();
+
+        }
+    }
+}
