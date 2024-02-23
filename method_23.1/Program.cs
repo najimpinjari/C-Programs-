d169a371c175a41e44a29e9a410edbe946404c6e
NAJIMdp41 <143899549+najimpinjari@users.noreply.github.com>
2024-02-23 19:11:11 +0530

    Add files via upload

---
+++ method_23.1/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace method_23._1
+{
+    class Program
+    {
+        public static void Myname()
+        {
+            Console.WriteLine("my name is najim");
+        }
+
+        static void Main(string[] args)
+        {
+            //Studnet s1 = new Studnet();
+            //s1.rollnumber = 1;
+            //s1.name = "rupesh";
+
+            //Console.WriteLine($"{s1.rollnumber} {s1.name}");
+                 
+            Myname();
+
+            //Program rupesh = new Program();
+           // rupesh.Myname();
+
+            Console.ReadKey();
+
+        }
+
+
+
+    }
+    class Studnet
+    {
+        
+        //public int rollnumber;
+
+        //public string name;
+
+    }
+
+
+
+}
