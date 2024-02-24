59c4e3f020c2dd4ee6679b0001146d6bdc000991
NAJIMdp41 <143899549+najimpinjari@users.noreply.github.com>
2024-02-24 17:50:44 +0530

    Add files via upload

---
+++ csharp_24/Program.cs
@@ -0,0 +1,50 @@
+﻿using system;
+using system.collections.generic;
+using system.linq;
+using system.text;
+using system.threading.tasks;
+
+namespace csharp_24
+{
+     class program
+    {
+
+        public static void name(string fname)
+        {
+            console.writeline(fname + " pinjari");
+        }
+        static void printname(string name)
+        {
+            console.writeline($" hello  {name}");
+        }
+
+        //method with return statment 
+        static string printmyname(string fn , string ln)
+        //if your method is retur something then you you have to use return statment in the mmethod
+        {
+            return fn + ln; 
+        }
+
+        static void main(string[] args)
+        {
+
+            name("najim");
+
+            printname("rupesh");
+
+            string rup = "rupesh";
+            printname(rup);
+
+
+            //calling method with return statemnt 
+            //if your method is retur something then you you have to use return statment in the mmethod
+            //and you want to see the result of a calling method then you must create a varibale to store a value of method 
+            //and store the value of method 
+            string result = printmyname("najim", "pinjari");
+            console.writeline(result);
+
+            console.readkey();
+
+        }
+    }
+}
