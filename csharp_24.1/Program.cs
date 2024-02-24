30c1068c76bf3089a278aee83e46acd956f51cb1
NAJIMdp41 <143899549+najimpinjari@users.noreply.github.com>
2024-02-24 17:51:40 +0530

    Add files via upload

---
+++ csharp_24.1/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace csharp_24._1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 200;
+            Console.WriteLine(a);
+            PrintValue(ref a);
+            Console.WriteLine(a);
+
+            Console.ReadKey();
+
+
+        }
+
+        static void PrintValue( ref int b )
+        {
+            b = 100;
+        }
+
+    }
+}
