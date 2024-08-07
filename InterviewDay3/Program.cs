759cfd3a3979f15cfa02d58611c62fbae3045dd2
NAJIMdp41 <143899549+najimpinjari@users.noreply.github.com>
2024-08-07 23:00:19 +0530

    Add files via upload

---
+++ InterviewDay3/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InterviewDay3
+{
+    public class Program
+    {
+        public static bool Isprime( int num)
+        {
+            if (num <= 1) 
+                return false;
+                for (int i = 2; i < num; i++)
+                {
+                    if (num % i == 0)
+                    return false;
+                }
+                return true;
+
+        } 
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter Your number");
+            int number = int.Parse(Console.ReadLine());
+
+            if (Isprime(number))
+            {
+                Console.WriteLine($" {number} is a prime number ");
+
+            }
+            else
+            {
+                Console.WriteLine($" {number} is a not a prime number");
+            }
+
+            Console.ReadLine();
+        }
+
+       
+    }
+}
